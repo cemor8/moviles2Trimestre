cdcf3c5142664b33e5c718918e44c439c56a1a5e
Carlos Morais Blanco <carlosmoraisblanco@gmail.com>
2024-02-02 20:19:23 +0100

    todo

--- sql/crearBiblioteca.sql
+++ sql/crearBiblioteca.sql
@@ -4,7 +4,6 @@ create table libro(
 	titulo varchar(30) not null,
     autor varchar(30),
     paginas int,
-    fecha date,
     primary key (titulo)
 );
-insert into libro values("Dune","Frank Herbert",412,"1965-05-02");
+insert into libro values("Dune","Frank Herbert",412);
